7c0c327805a485f34eff96a3986de354bea5a48d
cintiaMillacura22207 <37818974+cintiaMillacura2207@users.noreply.github.com>
2025-01-26 10:53:41 -0300

    define funcion agrega amigo

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 // definir variable de tipo array para almacenar los nombres de los amigos ingresados
 let amigos = [];
+
+// funcion para agregar amigos
+function agregarAmigo() {
+    let entrada = document.getElementById('amigo').value;
+    console.log(entrada);
+
+    if (entrada == '') {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        amigos.push(entrada);
+        console.log(amigos);
+        actualizarListaAmigos();
+        limpiarCaja();
+    }
+    return;
+}
